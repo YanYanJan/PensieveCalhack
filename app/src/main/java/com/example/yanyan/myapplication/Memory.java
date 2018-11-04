04ff74b559cf82e359010d67f8bed026da8b7858
Xiaoyan Kang <kang_xiaoyan@berkeley.edu>
2018-11-03 23:34:21 -0700

    Implemented floating action icon(s) with animation; Added icons for respective actions

--- app/src/main/java/com/example/yanyan/myapplication/Memory.java
+++ app/src/main/java/com/example/yanyan/myapplication/Memory.java
@@ -1,5 +1,9 @@
 package com.example.yanyan.myapplication;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,7 +18,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AnimationSet;
+import android.view.animation.BounceInterpolator;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by yanyan on 11/3/18.
@@ -23,7 +33,10 @@ import android.widget.TextView;
 public class Memory extends AppCompatActivity {
     private TextView mTextMessage;
     private Context mContext;
-    FloatingActionButton create;
+
+    private boolean isMenuOpen = false;
+    private int[] res = {R.id.create, R.id.sketchMem, R.id.cameraMem, R.id.audioMem, R.id.textMem};
+    private List<ImageView> imageViewList = new ArrayList<>();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -50,21 +63,56 @@ public class Memory extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.memory);
         mContext = this;
-        create = findViewById(R.id.create);
 
-        create.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent createIntent = new Intent(mContext, Create.class);
-                startActivity(createIntent);
-            }
-
-        });
-
+        for (int i = 0; i < res.length; i++) {
+            ImageView imageView = (ImageView)findViewById(res[i]);
+            imageViewList.add(imageView);
+        }
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    public void clickView(View view) {
+        switch (view.getId()) {
+            case R.id.create:
+                if (!isMenuOpen) {
+                    startAnim();
+                } else {
+                    closeAnim();
+                }
+                break;
+            case R.id.cameraMem:
+                Intent cameraIntent = new Intent(mContext, Create.class);
+                startActivity(cameraIntent);
+        }
 
+    }
+
+    private void startAnim() {
+        for (int i = 1; i < res.length; i++) {
+            imageViewList.get(i).setVisibility(View.VISIBLE);
+            float angle = (90 + 180 * 1.0f / (res.length - 2) * (i - 1));
+            PropertyValuesHolder h1 = PropertyValuesHolder.ofFloat("translationX", 0, (float) Math.sin(angle * 1.57 / 90) * 200);
+            PropertyValuesHolder h2 = PropertyValuesHolder.ofFloat("translationY", 0, (float) Math.cos(angle * 1.57 / 90) * 200);
+            ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(imageViewList.get(i), h1, h2);
+            animator.setDuration(1000);
+            animator.setFrameDelay(500 * i);
+            animator.setInterpolator(new BounceInterpolator());
+            animator.start();
+            isMenuOpen = true;
+        }
+    }
+
+    private void closeAnim() {
+        for (int i = 1; i < res.length; i++) {
+            float angle = (90 + 180 * 1.0f / (res.length - 2) * (i - 1));
+            PropertyValuesHolder h1 = PropertyValuesHolder.ofFloat("translationX",  (float) Math.sin(angle * 1.57 / 90) * 200, 0);
+            PropertyValuesHolder h2 = PropertyValuesHolder.ofFloat("translationY", (float) Math.cos(angle * 1.57 / 90) * 200, 0);
+            ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(imageViewList.get(i), h1, h2);
+            animator.setDuration(500);
+            animator.start();
+            isMenuOpen = false;
+        }
+    }
 }
